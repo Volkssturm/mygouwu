994dc544823bf006d12ad32f2d5ff0836afbdb8b
Volkssturm <15346585791@sina.cn>
2017-06-30 09:25:53 +0800

    first commit

---
+++ src/main/java/com/example/mygouwu/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.example.mygouwu;
+
+import android.app.Activity;
+
+/**
+ * com.example.mygouwu
+ * 徐世辉  1503A
+ * 类作用：本类---
+ * 思路：
+ * 1.
+ * 2.
+ * 3.
+ * 2017/6/28 14:16
+ */
+
+class BaseActivity extends Activity{
+}
